b0ac2ab26f4005d38d4e4ed4aaa58ca2817a89c7
Alex Radevich <virtuain98@gmail.com>
2016-10-15 20:29:20 +0300

    Some errors fixed

--- Error.cpp
+++ Error.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "Error.h"
 
 namespace Error
@@ -11,21 +11,41 @@ namespace Error
 		ERROR_ENTRY_NODEF( 6 ), ERROR_ENTRY_NODEF( 7 ), ERROR_ENTRY_NODEF( 8 ), ERROR_ENTRY_NODEF( 9 ),
 		ERROR_ENTRY_NODEF10( 10 ), ERROR_ENTRY_NODEF10( 20 ), ERROR_ENTRY_NODEF10( 30 ), ERROR_ENTRY_NODEF10( 40 ), ERROR_ENTRY_NODEF10( 50 ),
 		ERROR_ENTRY_NODEF10( 60 ), ERROR_ENTRY_NODEF10( 70 ), ERROR_ENTRY_NODEF10( 80 ), ERROR_ENTRY_NODEF10( 90 ),
-		ERROR_ENTRY_NODEF100( 100, "Параметр -in должен быть задан"),
+		ERROR_ENTRY( 100, "Параметр -in должен быть задан"),
 		ERROR_ENTRY_NODEF( 101 ), ERROR_ENTRY_NODEF( 102 ), ERROR_ENTRY_NODEF( 103 ),
-		ERROR_ENTRY_NODEF( 104, "Превышена длина входного параметра" ),
+		ERROR_ENTRY( 104, "Превышена длина входного параметра" ),
 		ERROR_ENTRY_NODEF( 105 ), ERROR_ENTRY_NODEF( 106 ), ERROR_ENTRY_NODEF( 107 ),
 		ERROR_ENTRY_NODEF( 108), ERROR_ENTRY_NODEF( 109 ), 
-		ERROR_ENTRY_NODEF( 110, "Ошибка при открытии файла с мсходным кодом" ),
-		ERROR_ENTRY_NODEF( 111, "Недопустимый симво в исходном файле (-in)" ),
-		ERROR_ENTRY_NODEF( 112, "Ошибка при создании файла протокола (-log)"),
+		ERROR_ENTRY( 110, "Ошибка при открытии файла с исходным кодом" ),
+		ERROR_ENTRY( 111, "Недопустимый символ в исходном файле (-in)" ),
+		ERROR_ENTRY( 112, "Ошибка при создании файла протокола (-log)"),
 		ERROR_ENTRY_NODEF( 113 ), ERROR_ENTRY_NODEF( 114 ), ERROR_ENTRY_NODEF( 115 ), 
 		ERROR_ENTRY_NODEF( 116 ), ERROR_ENTRY_NODEF( 117 ), ERROR_ENTRY_NODEF( 118 ), ERROR_ENTRY_NODEF( 119 ),
-		ERROR_ENTRY_NODEF10( 120 ), ERROR_ENTRY_NODEF10( 130 ), ERROR_ENTRY_NODEF( 140 ), ERROR_ENTRY_NODEF( 150 ), 
-		ERROR_ENTRY_NODEF( 160 ), ERROR_ENTRY_NODEF( 170 ), ERROR_ENTRY_NODEF( 180 ), ERROR_ENTRY_NODEF( 190 ),
-		ERROR_ENTRY_NODEF100( 200 ), ERROR_ENTRY_NODEF100( 300 ), ERROR_ENTRY_NODEF( 400 ), ERROR_ENTRY_NODEF( 500 ),
-		ERROR_ENTRY_NODEF( 600 ), ERROR_ENTRY_NODEF( 700 ), ERROR_ENTRY_NODEF( 800 ), ERROR_ENTRY_NODEF( 900 )
+		ERROR_ENTRY_NODEF10( 120 ), ERROR_ENTRY_NODEF10( 130 ), ERROR_ENTRY_NODEF10( 140 ), ERROR_ENTRY_NODEF10( 150 ), 
+		ERROR_ENTRY_NODEF10( 160 ), ERROR_ENTRY_NODEF10( 170 ), ERROR_ENTRY_NODEF10( 180 ), ERROR_ENTRY_NODEF10( 190 ),
+		ERROR_ENTRY_NODEF100( 200 ), ERROR_ENTRY_NODEF100( 300 ), ERROR_ENTRY_NODEF100( 400 ), ERROR_ENTRY_NODEF100( 500 ),
+		ERROR_ENTRY_NODEF100( 600 ), ERROR_ENTRY_NODEF100( 700 ), ERROR_ENTRY_NODEF100( 800 ), ERROR_ENTRY_NODEF100( 900 )
 	};
+
+	ERROR geterror(int id) 
+	{
+		ERROR error;
+		error.id = id;
+		//error.message = ERROR_ENTRY_NODEF100()
+		return error;
+	}
+
+	ERROR geterrorin(int id, int line = -1, int col = -1)
+	{
+		ERROR error;
+		error.id = id;
+		return error;
+	}
+}
+
+int main() 
+{
+	return 0;
 }
 
 
