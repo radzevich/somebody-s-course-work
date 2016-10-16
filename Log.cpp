a843699b8020905ec1bb0294fcf212c2f1ed17bb
Alex Radevich <virtuain98@gmail.com>
2016-10-16 15:59:29 +0300

    function getLog created

--- Log.cpp
+++ Log.cpp
@@ -1,19 +1,20 @@
 #include "Log.h"
 #include <fstream>
 
-namespace Log {
+namespace Log 
+{
 	LOG getlog( wchar_t logfile[] )
 	{
-		try
-		{
-			LOG log;
-			std::ofstream fout;
-			fout.open( logfile );
-			wcscpy(log.logfile, logfile);
-			log.stram = &fout;
+		LOG log;
+		std::ofstream fout;
+		fout.open( logfile );
+		wcscpy(log.logfile, logfile);
+		log.stram = &fout;
+		if (NULL == &fout)
 			throw  ERROR_THROW(LOG_FILE_CREATING_ERROR_ID);
-		}
 	}
 
 
+
+	
 }
